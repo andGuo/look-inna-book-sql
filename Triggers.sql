b7367c5fff04da5402033734e9178389ad84a0c5
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-05 01:22:19 -0500

    making triggers is hard

--- Triggers.sql
+++ Triggers.sql
@@ -1,46 +1,84 @@
 -- inserts a ROW INTO public.users
-CREATE OR REPLACE FUNCTION public.handle_new_user()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-SECURITY definer SET search_path = PUBLIC
-AS $$
-BEGIN
-  INSERT INTO public.profiles (profile_id, first_name, last_name)
-  VALUES (new.id, NULL, NULL);
-  RETURN NEW;
+CREATE
+OR REPLACE FUNCTION public.handle_new_user() RETURNS TRIGGER LANGUAGE plpgsql SECURITY definer
+SET
+  search_path = PUBLIC AS $$ BEGIN
+INSERT INTO
+  public.profiles (profile_id, first_name, last_name)
+VALUES
+  (new.id, NULL, NULL);
+
+RETURN NEW;
+
 END;
+
 $$;
 
 DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+
 -- TRIGGER the FUNCTION every TIME a user IS created
 CREATE TRIGGER on_auth_user_created
-  AFTER INSERT ON auth.users
-  FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
+AFTER
+INSERT
+  ON auth.users FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
 
 -- Cart Stuff
-CREATE OR REPLACE FUNCTION public.create_cart()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO public.carts(profile_id) VALUES (NEW.profile_id);
-    RETURN NEW;
+CREATE
+OR REPLACE FUNCTION public.create_cart() RETURNS TRIGGER LANGUAGE plpgsql AS $$ BEGIN
+INSERT INTO
+  public.carts(profile_id)
+VALUES
+  (NEW.profile_id);
+
+RETURN NEW;
+
 END;
+
 $$;
 
 DROP TRIGGER IF EXISTS on_profile_created ON public.profiles;
-CREATE TRIGGER on_profile_created
-    AFTER INSERT ON public.profiles
-    REFERENCING NEW TABLE AS new_profiles
-    FOR EACH ROW EXECUTE PROCEDURE public.create_cart();
--- these don't work
-CREATE OR REPLACE FUNCTION public.update_carts()
 
+CREATE TRIGGER on_profile_created
+AFTER
+INSERT
+  ON public.profiles REFERENCING NEW TABLE AS new_profiles FOR EACH ROW EXECUTE PROCEDURE public.create_cart();
+
+CREATE
+OR REPLACE FUNCTION public.update_carts() RETURNS TRIGGER LANGUAGE plpgsql AS $$ BEGIN
+UPDATE
+  carts
+SET
+  carts.cart_total = (
+    SELECT
+      cart_id
+    FROM
+      (
+        SELECT
+          cart_id,
+          SUM(quantity * msrp) as cart_total,
+          SUM(quantity) as total_quantity
+        FROM
+          cart_books
+          JOIN books ON cart_books.isbn = books.isbn
+        GROUP BY
+          cart_id
+      ) as t
+    WHERE
+      carts.profile_id = t.cart_id;
+
+) RETURN NEW;
+
+END;
+
+$$;
+
+DROP TRIGGER IF EXISTS on_cart_book_added ON public.cart_books;
 
 CREATE TRIGGER on_cart_book_added
-    AFTER INSERT OR UPDATE OR DELETE ON public.cart_books
-    FOR EACH ROW EXECUTE PROCEDURE public.update_carts
+AFTER
+INSERT
+  OR
+UPDATE
+  OR DELETE ON public.cart_books EXECUTE PROCEDURE public.update_carts();
 
-
-  -- Set up Book Image Storage:
-  
+-- Set up Book Image Storage:
